92b63392bd2e02a798a3f5b4155f77b31315754b
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-01-31 02:12:23 -0800

    Add blur to main section when menu is active and increase font size of menu items

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const disappear = document.querySelector('#disappear');
 const rotateRight = document.querySelector('#rotate-right');
 const rotateLeft = document.querySelector('#rotate-left');
 const menuList = document.querySelector('#menuList');
-const menuButtons = document.querySelectorAll('.menuButton');
+const menuButton1 = document.querySelector('#menuButton1');
+const menuButton2 = document.querySelector('#menuButton2');
+const menuButton3 = document.querySelector('#menuButton3');
+const mainSection = document.querySelector('#main-section');
+const logo = document.querySelector('#logo');
 let activeMenu = false;
 hamburger.addEventListener('click', () => {
   if (activeMenu === false) {
-    menuList.style.marginTop = '0';
+    menuList.style.top = '0';
     disappear.style.display = 'none';
     rotateRight.style.transform = 'rotate(45deg) translateX(20%) translateY(250%)';
     rotateLeft.style.transform = 'rotate(-45deg) translateX(-20%)';
@@ -16,8 +20,11 @@ hamburger.addEventListener('click', () => {
     rotateRight.style.borderColor = '#fff';
     rotateLeft.style.borderColor = '#fff';
     activeMenu = true;
+    logo.style.color = '#fff';
+    hamburger.style.backgroundColor = '#6070ff';
+    mainSection.style.filter = 'blur(6px)';
   } else {
-    menuList.style.marginTop = '-200vh';
+    menuList.style.top = '-200vh';
     disappear.style.display = 'block';
     rotateRight.style.transform = 'rotate(0deg) translateX(0%) translateY(0%)';
     rotateLeft.style.transform = 'rotate(0deg) translateX(0%)';
@@ -25,11 +32,37 @@ hamburger.addEventListener('click', () => {
     rotateLeft.style.backgroundColor = '#6070ff';
     rotateRight.style.borderColor = '#6070ff';
     rotateLeft.style.borderColor = '#6070ff';
+    mainSection.style.filter = 'blur(0)';
+    logo.style.color = '#6070ff';
     activeMenu = false;
   }
 });
-menuButtons.addEventListener('click', () => {
-  menuList.style.marginTop = '-200vh';
+
+menuButton1.addEventListener('click', () => {
+  menuList.style.top = '-200vh';
+  disappear.style.display = 'block';
+  rotateRight.style.transform = 'rotate(0deg) translateX(0%) translateY(0%)';
+  rotateLeft.style.transform = 'rotate(0deg) translateX(0%)';
+  rotateRight.style.backgroundColor = '#6070ff';
+  rotateLeft.style.backgroundColor = '#6070ff';
+  rotateRight.style.borderColor = '#6070ff';
+  rotateLeft.style.borderColor = '#6070ff';
+  activeMenu = false;
+});
+
+menuButton2.addEventListener('click', () => {
+  menuList.style.top = '-200vh';
+  disappear.style.display = 'block';
+  rotateRight.style.transform = 'rotate(0deg) translateX(0%) translateY(0%)';
+  rotateLeft.style.transform = 'rotate(0deg) translateX(0%)';
+  rotateRight.style.backgroundColor = '#6070ff';
+  rotateLeft.style.backgroundColor = '#6070ff';
+  rotateRight.style.borderColor = '#6070ff';
+  rotateLeft.style.borderColor = '#6070ff';
+  activeMenu = false;
+});
+menuButton3.addEventListener('click', () => {
+  menuList.style.top = '-200vh';
   disappear.style.display = 'block';
   rotateRight.style.transform = 'rotate(0deg) translateX(0%) translateY(0%)';
   rotateLeft.style.transform = 'rotate(0deg) translateX(0%)';
