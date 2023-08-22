1f0f5f1e53276b7f4185701cf773a32f4846535c
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-08-23 00:37:34 +0100

    Redesign cards section

--- modules/class.js
+++ modules/class.js
@@ -24,12 +24,14 @@ class Cards {
 
 export const card1 = new Cards(
   'Music Festival',
-  `This capstone project is about a music festival in my country.
-  I created it in honor of my favorite artiste, Wizkid.`,
-  "'./images/mil.jpg'",
+  `Experience the ultimate Wizkid Musical Festival journey through our digital platform.
+   Get in-depth insights into the upcoming festival in Lagos, Nigeria.
+    Discover featured artistes, event schedules, and venue details. Seamlessly book your tickets, explore interactive maps,
+   and share the excitement with friends. Elevate your festival experience with our all-inclusive hub.`,
+  "'./images/card-images/mil.png'",
   ['CANOPY', 'Front End Dev', '2015'],
   ['"canopy"', '"bed"', '"year"'],
-  ['HTML', 'CSS', 'JavaScript'],
+  ['JavaScript','HTML', 'CSS','Git','GitHub'],
   '"https://david-lanzz.github.io/My-First-Capstone/"',
   '"https://github.com/David-Lanzz/My-First-Capstone"',
   {
@@ -37,7 +39,7 @@ export const card1 = new Cards(
     stack1: 'CANOPY',
     stack2: 'Front End Dev',
     stack3: '2023',
-    url: './images/mil.jpg',
+    url: './images/card-images/mil.png',
     description: `This capstone project is about a music festival in my country.
    I created it in honor of my favorite artiste, Wizkid. The project has many features including of performing artiste cards
    using javascript, speed and responsiveness on all devices and many more.To preview, use the live link below`,
@@ -47,16 +49,16 @@ export const card2 = new Cards(
   'ToDo List',
   `This project is containing a ToDo list which i used to sharpen my skills in
    JavaScript and also to learn how to use webpacks efficiently.`,
-  '"./images/todo.jpg"',
+  '"./images/card-images/todo.png"',
   ['TODO', 'Lead Developer', '2018'],
   ['"canopy"', '"bed"', '"year"'],
-  ['HTML', 'Webpack', 'CSS', 'JavaScript'],
+  ['JavaScript','Webpack','HTML', 'CSS','Git','GitHub'],
   '"https://david-lanzz.github.io/My-ToDo-List/dist/"',
   '"https://github.com/David-Lanzz/My-ToDo-List"',
   {
     description: `This project is containing a ToDo list which i used to sharpen my skills in JavaScript and also to learn how to use webpacks efficiently. The project consists of different features
          including adding, editing, deleting and creating of tasks dynamically using javascript.Multiple tasks can also be cleared at once.`,
-    url: './images/todo.jpg',
+    url: './images/card-images/todo.png',
     header: 'ToDo List',
     stack1: 'TODO',
     stack2: 'Lead Developer',
@@ -70,7 +72,7 @@ export const card3 = new Cards(
   '"./images/bookstore.jpg"',
   ['TODO', 'Lead Developer', '2018'],
   ['"canopy"', '"bed"', '"year"'],
-  ['React','CSS','JavaScript','Redux/TK','API','React-Router'],
+  ['JavaScript','React','Redux','API','CSS','React-Router','Git','GitHub'],
   '"https://bookstore-pfc6.onrender.com/"',
   '"https://github.com/David-Lanzz/Bookstore/"',
   {
@@ -87,16 +89,16 @@ export const card4 = new Cards(
   'COVID STATS',
   `This is a project which allows users have access to the data statistics of COVID cases per country.
   `,
-  '"./images/covid.jpg"',
+  '"./images/card-images/covid.png"',
   ['METRICS', 'Lead Developer', '2023'],
   ['"canopy"', '"bed"', '"year"'],
-  ['React', 'CSS', 'JavaScript','API','Redux/TK','React-Router'],
+  ['JavaScript','React','Redux','API','React-Router','CSS','Git','GitHub'],
   '"https://mapmetrics.onrender.com/"',
   '"https://github.com/David-Lanzz/map-metrics/"',
   {
     description: `With all due respect to lives lost during the COVID 19 pandemic,This project allows users zccross the globe have access to
     the data statistics of COVID cases per country. The project displays the names of countries and he states data of confirmed cases and deaths recorded.`,
-    url: './images/covid.jpg',
+    url: './images/card-images/covid.png',
     header: 'COVID 19 MAP METRICS',
     stack1: 'METRICS',
     stack2: 'Lead Developer',
@@ -107,15 +109,15 @@ export const card5 = new Cards(
   'CALCULATOR',
   `This is a project which allows users perform basic calculations in mathematics while also displaying quotes given by an API.
   `,
-  '"./images/math_magicians.jpg"',
+  '"./images/card-images/math.png"',
   ['MATHS', 'Lead Developer', '2023'],
   ['"canopy"', '"bed"', '"year"'],
-  ['HTML', 'React', 'CSS', 'JavaScript','React-Router'],
+  ['JavaScript','React','React-Router','API','HTML','CSS','Git','GitHub'],
   '"https://mathmagicians-4omq.onrender.com/"',
   '"https://github.com/David-Lanzz/math-magicians/"',
   {
     description: 'Ever used a calculator with quotes before? I guess not...This is a project which allows users perform basic calculations in mathematics while also displaying quotes given by an API',
-    url: './images/math_magicians.jpg',
+    url: './images/card-images/math.png',
     header: 'MATH MAGICIANS',
     stack1: 'MATHS',
     stack2: 'Lead Developer',
@@ -126,17 +128,17 @@ export const card6 = new Cards(
   'KONAGRILL',
   `This is a project which different delicacies offered by a beef restaurant.
   `,
-  '"./images/konagrill.jpg"',
+  '"./images/card-images/kona.png"',
   ['COMMERCE', 'Lead Developer', '2023'],
   ['"canopy"', '"bed"', '"year"'],
-  ['HTML', 'Webpack', 'CSS', 'JavaScript','API'],
+  ['JavaScript','API','Webpack','HTML','CSS','Git','GitHub'],
   '"https://david-lanzz.github.io/Kanban/dist/"',
   '"https://github.com/David-Lanzz/Kanban"',
   {
     description: `This is a project which different delicacies offered by a beef restaurant.
       All meals data are gotten from the company's API and displayed using Object Oriented programming.
       Engagements like comments and reactions are also allowed`,
-    url: './images/konagrill.jpg',
+    url: './images/card-images/kona.png',
     header: 'KONAGRILL MEALS',
     stack1: 'BEEF',
     stack2: 'Lead Developer',
@@ -147,15 +149,15 @@ export const card7 = new Cards(
   'LEADERBOARD',
   `This is a project which displays a user leaderboard in a particular game.Users can store scores and view scores of other users.
   `,
-  '"./images/leaderboard.jpg"',
+  '"./images/card-images/leaderboard.png"',
   ['SCORES', 'Lead Developer', '2023'],
   ['"canopy"', '"bed"', '"year"'],
-  ['HTML', 'Webpack', 'CSS', 'JavaScript','API'],
+  ['JavaScript','API','Webpack','HTML','CSS','Git','GitHub'],
   '"https://david-lanzz.github.io/Leaderboard-Project/dist/"',
   '"https://github.com/David-Lanzz/Leaderboard-Project"',
   {
     description: 'This is a project which displays a user leaderboard in a particular game.Users can store scores and view scores of other users',
-    url: './images/leaderboard.jpg',
+    url: './images/card-images/leaderboard.png',
     header: 'LEADERBOARD',
     stack1: 'SCORES',
     stack2: 'Lead Developer',
@@ -166,15 +168,15 @@ export const card8 = new Cards(
   'E-COMMERCE',
   `UNDER CONSTRUCTION!.
   `,
-  '"./images/e_commerce.jpg"',
+  '"./images/card-images/commerce.png"',
   ['COMMERCE', 'Lead Developer', '2023'],
   ['"canopy"', '"bed"', '"year"'],
-  ['CSS', 'React', 'Vite', 'JavaScript','React Router','Redux/TK','API'],
+  ['JavaScript','React','Redux','Vite','React Router','API','Git','GitHub','Figma'],
   '"https://myecommerceapp-1nbf.onrender.com/"',
   '"https://github.com/David-Lanzz/ecommerceapp/"',
   {
     description: 'UNDER CONSTRUCTION!',
-    url: './images/e_commerce.jpg',
+    url: './images/card-images/commerce.png',
     header: 'E-COMMERCE',
     stack1: 'SCORES',
     stack2: 'Lead Developer',
@@ -186,15 +188,15 @@ export const card9 = new Cards(
   `This is a project built by me and my team of developers to help users all
   around the world book doctors according to their availability and convenience.
   `,
-  '"images\doc.PNG"',
+  '"./images/card-images/doc.png"',
   ['HEALTH', 'Lead Developer', '2023'],
   ['"canopy"', '"bed"', '"year"'],
-  ['TailWindCSS', 'React', 'Ruby on Rails', 'API','JavaScript','Redux/TK','React Router'],
+  ['Ruby on Rails','React','Redux','API','JavaScript','TailWindCSS','React Router','Git','GitHub'],
   '"https://reserve-me-a-doc.onrender.com/"',
   '"https://github.com/ilyassatlassi/final-capstone-project-frontend"',
   {
     description: 'This is a project built by me and my team of developers to help users all around the world book doctors according to their availability and convenience.',
-    url: './images/doc.PNG',
+    url: './images/card-images/doc.png',
     header: 'HEALTH',
     stack1: 'BOOKING',
     stack2: 'Lead Developer',
--- script.js
+++ script.js
@@ -1,105 +1,133 @@
 /* eslint-disable array-callback-return */
 
 import {
-  card1, card2, card3, card4, card5, card6, card7, card8,card9
-} from './modules/class.js';
+  card1,
+  card2,
+  card3,
+  card4,
+  card5,
+  card6,
+  card7,
+  card8,
+  card9,
+} from "./modules/class.js";
 
-const disappear = document.querySelector('#disappear');
-const rotateRight = document.querySelector('#rotate-right');
-const rotateLeft = document.querySelector('#rotate-left');
-const menuList = document.querySelector('#menuList');
+const disappear = document.querySelector("#disappear");
+const rotateRight = document.querySelector("#rotate-right");
+const rotateLeft = document.querySelector("#rotate-left");
+const menuList = document.querySelector("#menuList");
 let activeMenu = false;
-const email = document.querySelector('#email');
-const emailError = document.querySelector('#emailError');
-const inputname = document.querySelector('#name');
-const tryme = document.querySelectorAll(".blurr")
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const inputname = document.querySelector("#name");
+const tryme = document.querySelectorAll(".blurr");
 
 const inputObj = {
-  inputEmail: '',
-  inputName: '',
+  inputEmail: "",
+  inputName: "",
 };
 
-window.addEventListener('load', () => {
-  let outputObj = '';
-  outputObj = JSON.parse(localStorage.getItem('object'));
-  if (inputname.value === '') {
+window.addEventListener("load", () => {
+  let outputObj = "";
+  outputObj = JSON.parse(localStorage.getItem("object"));
+  if (inputname.value === "") {
     inputname.value = outputObj.inputName;
     email.value = outputObj.inputEmail;
   }
 });
 
 const openMenu = () => {
-  menuList.style.top = '0';
-  disappear.style.display = 'none';
-  rotateRight.style.transform = 'rotate(45deg) translateX(20%) translateY(250%)';
-  rotateLeft.style.transform = 'rotate(-45deg) translateX(-20%)';
-  rotateRight.style.backgroundColor = '#fff';
-  rotateLeft.style.backgroundColor = '#fff';
-  rotateRight.style.borderColor = '#fff';
-  rotateLeft.style.borderColor = '#fff';
+  menuList.style.top = "0";
+  disappear.style.display = "none";
+  rotateRight.style.transform =
+    "rotate(45deg) translateX(20%) translateY(250%)";
+  rotateLeft.style.transform = "rotate(-45deg) translateX(-20%)";
+  rotateRight.style.backgroundColor = "#fff";
+  rotateLeft.style.backgroundColor = "#fff";
+  rotateRight.style.borderColor = "#fff";
+  rotateLeft.style.borderColor = "#fff";
   activeMenu = true;
 };
 const closeMenu = () => {
   activeMenu = false;
-  menuList.style.top = '-200vh';
-  disappear.style.display = 'block';
-  rotateRight.style.transform = 'rotate(0deg) translateX(0%) translateY(0%)';
-  rotateLeft.style.transform = 'rotate(0deg) translateX(0%)';
-  rotateRight.style.backgroundColor = '#6070ff';
-  rotateLeft.style.backgroundColor = '#6070ff';
-  rotateRight.style.borderColor = '#6070ff';
-  rotateLeft.style.borderColor = '#6070ff';
+  menuList.style.top = "-200vh";
+  disappear.style.display = "block";
+  rotateRight.style.transform = "rotate(0deg) translateX(0%) translateY(0%)";
+  rotateLeft.style.transform = "rotate(0deg) translateX(0%)";
+  rotateRight.style.backgroundColor = "#6070ff";
+  rotateLeft.style.backgroundColor = "#6070ff";
+  rotateRight.style.borderColor = "#6070ff";
+  rotateLeft.style.borderColor = "#6070ff";
 };
 
-menuList.addEventListener('click', (e) => {
+menuList.addEventListener("click", (e) => {
   if (
-    e.target.id === 'menuButton1'
-    || e.target.id === 'menuButton2'
-    || e.target.id === 'menuButton3'
+    e.target.id === "menuButton1" ||
+    e.target.id === "menuButton2" ||
+    e.target.id === "menuButton3"
   ) {
-    document.querySelector('body').classList.remove('no-overflow');
-    for(let i = 0;i < tryme.length;i++){
-      tryme[i].classList.remove("show-blur")
-     }
+    document.querySelector("body").classList.remove("no-overflow");
+    for (let i = 0; i < tryme.length; i++) {
+      tryme[i].classList.remove("show-blur");
+    }
     closeMenu();
   }
 });
 
-const works = document.querySelector('#works');
+const works = document.querySelector("#works");
 
-const projectCards = [card9, card1, card3, card4, card6, card5, card2, card7, card8];
+const projectCards = [
+  card9,
+  card1,
+  card3,
+  card4,
+  card6,
+  card5,
+  card2,
+  card7,
+  card8,
+];
 projectCards.map((card, index) => {
   card.id = index + 1;
   card.buttonId = index + 1;
-  card.hero = 'tonic';
+  card.hero = "tonic";
   if (index % 2 === 0) {
-    card.classes = 'projects';
+    card.classes = "projects";
   } else {
-    card.classes = 'projects reverse';
+    card.classes = "projects reverse";
   }
 });
 
 let defaultLoopValue = 4;
 
 function createCards() {
-  let techItems = '';
-  let languages = '';
-  if (techItems === '') {
+  let techItems = "";
+  let languages = "";
+  if (techItems === "") {
     for (let item = 0; item < defaultLoopValue; item += 1) {
-      if (languages === '') {
+      if (languages === "") {
         for (let i = 0; i < projectCards[item].technology.length; i += 1) {
           techItems += `<li class=${projectCards[item].techClass[i]}>${projectCards[item].technology[i]}</li>`;
         }
-        for (let i = 0; i < projectCards[item].languages.length; i += 1) {
-          languages += `<ul class = 'popuplang-ul'><li class = "html">${projectCards[item].languages[i]}</li></ul>`;
+        const screenWidth = window.innerWidth;
+        if (screenWidth <= 768) {
+          // Mobile screen size
+          for (let i = 0; i <= 3; i += 1) {
+            languages += `<ul class='popuplang-ul'><li class="html">${projectCards[item].languages[i]}</li></ul>`;
+          }
+        } else {
+          // Desktop screen size
+          for (let i = 0; i < projectCards[item].languages.length; i += 1) {
+            languages += `<ul class='popuplang-ul'><li class="html">${projectCards[item].languages[i]}</li></ul>`;
+          }
         }
       }
       const projects = `<div class= "${projectCards[item].classes}" id=${projectCards[item].id}><img class = 'img' src = ${projectCards[item].url}alt = 'project image'></img><ul class = 'projectInfo'><li class =${projectCards[item].hero}><h3>${projectCards[item].name}</h3></li><li class = 'techstack'><div>
-      <ul>${techItems}</ul></div></li><p class= 'aboutProject'>${projectCards[item].description}</p><li class = 'lang'>
+      <ul>${techItems}</ul></div></li><p class='aboutProject'>${projectCards[item].description}</p><li class = 'lang'>
       <ul>${languages}</ul><li class= 'view'><a href = #${projectCards[item].id} class = 'buttons project-buttons' id=${projectCards[item].buttonId}>
        See Project</a></li></ul></div>`;
-      techItems = '';
-      languages = '';
+      techItems = "";
+      languages = "";
       works.innerHTML += projects;
     }
   }
@@ -109,7 +137,7 @@ createCards();
 const displayPopup = (array, target) => {
   array.map((element) => {
     if (target.id.toString() === element.id.toString()) {
-      document.querySelector('.popup').innerHTML = `
+      document.querySelector(".popup").innerHTML = `
  <section class= "popup-container">
  <section class="window-popup">
  <ul class="popup-image">
@@ -138,7 +166,7 @@ const displayPopup = (array, target) => {
     <li class="popuplang lang">
         <ul class="popuplang-ul popup-justify" id="popuplang">
 ${element.languages.map((language) => {
-  return `<li class="html">${language}</li>`
+  return `<li class="html">${language}</li>`;
 })}
         </ul>
         <div class="thinline"></div>
@@ -146,12 +174,12 @@ ${element.languages.map((language) => {
             <li class="popupBtn">
                 <div id="livelink">
 <a href=${
-  element.live
-} class="buttons">See live<img src=./images/Icon.png alt="github icon"></a>
+        element.live
+      } class="buttons">See live<img src=./images/Icon.png alt="github icon"></a>
                 </div>
                 <div id="sourcelink"> <a href=${
-  element.source
-} class="buttons">See Source<img src="./images/Vector22.png"
+                  element.source
+                } class="buttons">See Source<img src="./images/Vector22.png"
    alt="github icon"></a>
                 </div>
             </li>
@@ -165,101 +193,118 @@ ${element.languages.map((language) => {
   });
 };
 
-window.addEventListener('click', (e) => {
+window.addEventListener("click", (e) => {
   const { target } = e;
   if (
-    target.id === 'menu'
-    || target.id === 'rotate-right'
-    || target.id === 'rotate-left'
-    || target.id === 'disappear'
+    target.id === "menu" ||
+    target.id === "rotate-right" ||
+    target.id === "rotate-left" ||
+    target.id === "disappear"
   ) {
     if (activeMenu === false) {
       openMenu();
-      document.querySelector('body').classList.add('no-overflow');
-      for(let i = 0;i < tryme.length;i++){
-        tryme[i].classList.add("show-blur")
-       }
+      document.querySelector("body").classList.add("no-overflow");
+      for (let i = 0; i < tryme.length; i++) {
+        tryme[i].classList.add("show-blur");
+      }
     } else if (activeMenu === true) {
-      for(let i = 0;i < tryme.length;i++){
-        tryme[i].classList.remove("show-blur")
-       }
-       document.querySelector('body').classList.remove('no-overflow');
+      for (let i = 0; i < tryme.length; i++) {
+        tryme[i].classList.remove("show-blur");
+      }
+      document.querySelector("body").classList.remove("no-overflow");
       closeMenu();
     }
   }
-  if (target.id === 'langs-dropdown' || target.id === 'langs') {
-    document.querySelector('.plangsContainer').classList.toggle('move-away');
-    document.querySelector('.v').classList.toggle('v-active');
-  } else if (target.id === 'framework-dropdown' || target.id === 'framework') {
-    document.querySelector('.frameworkContainer').classList.toggle('move-away');
-    document.querySelector('.frame-v').classList.toggle('v-active');
-  } else if (target.id === 'skills-dropdown' || target.id === 'skills') {
-    document.querySelector('.skillsContainer').classList.toggle('move-away');
-    document.querySelector('.skill-v').classList.toggle('v-active');
+  if (target.id === "langs-dropdown" || target.id === "langs") {
+    document.querySelector(".plangsContainer").classList.toggle("move-away");
+    document.querySelector(".v").classList.toggle("v-active");
+  } else if (target.id === "framework-dropdown" || target.id === "framework") {
+    document.querySelector(".frameworkContainer").classList.toggle("move-away");
+    document.querySelector(".frame-v").classList.toggle("v-active");
+  } else if (target.id === "skills-dropdown" || target.id === "skills") {
+    document.querySelector(".skillsContainer").classList.toggle("move-away");
+    document.querySelector(".skill-v").classList.toggle("v-active");
   }
-  if(target.id === 'langs-dropdown' || target.id === 'langs' ||
-  target.id === 'framework-dropdown' || target.id === 'framework'
-  || target.id === 'skills-dropdown' || target.id === 'skills'
-   ) {
-    if(document.querySelector('.plangsContainer').classList.contains('move-away') === false
-    || document.querySelector('.frameworkContainer').classList.contains('move-away') === false
-    || document.querySelector('.skillsContainer').classList.contains('move-away') === false
+  if (
+    target.id === "langs-dropdown" ||
+    target.id === "langs" ||
+    target.id === "framework-dropdown" ||
+    target.id === "framework" ||
+    target.id === "skills-dropdown" ||
+    target.id === "skills"
+  ) {
+    if (
+      document
+        .querySelector(".plangsContainer")
+        .classList.contains("move-away") === false ||
+      document
+        .querySelector(".frameworkContainer")
+        .classList.contains("move-away") === false ||
+      document
+        .querySelector(".skillsContainer")
+        .classList.contains("move-away") === false
     ) {
-      document.querySelector('.lanzz').style.display = "none"
-    }
-    else if(document.querySelector('.plangsContainer').classList.contains('move-away') === true
-    || document.querySelector('.frameworkContainer').classList.contains('move-away') === true
-    || document.querySelector('.skillsContainer').classList.contains('move-away') === true
+      document.querySelector(".lanzz").style.display = "none";
+    } else if (
+      document
+        .querySelector(".plangsContainer")
+        .classList.contains("move-away") === true ||
+      document
+        .querySelector(".frameworkContainer")
+        .classList.contains("move-away") === true ||
+      document
+        .querySelector(".skillsContainer")
+        .classList.contains("move-away") === true
     ) {
-      document.querySelector('.lanzz').style.display = "block"
+      document.querySelector(".lanzz").style.display = "block";
     }
-   }
+  }
 
-  if (target.className.includes('project-buttons')) {
+  if (target.className.includes("project-buttons")) {
     e.preventDefault();
     displayPopup(projectCards, target);
-    document.querySelector('.blur').classList.add('show-blur');
-    document.querySelector('body').classList.add('no-overflow');
+    document.querySelector(".blur").classList.add("show-blur");
+    document.querySelector("body").classList.add("no-overflow");
     if (window.screen.width < 710) {
-      document.querySelector('.popup-container').classList.add('display-popup');
+      document.querySelector(".popup-container").classList.add("display-popup");
     } else {
       document
-        .querySelector('.popup-container')
-        .classList.add('display-popup-desktop');
+        .querySelector(".popup-container")
+        .classList.add("display-popup-desktop");
     }
   }
-  if (target.id === 'submit' && email.value !== email.value.toLowerCase()) {
+  if (target.id === "submit" && email.value !== email.value.toLowerCase()) {
     e.preventDefault();
-    emailError.style.visibility = 'visible';
+    emailError.style.visibility = "visible";
     setTimeout(() => {
-      emailError.style.visibility = 'hidden';
+      emailError.style.visibility = "hidden";
     }, 2000);
   } else if (
-    target.id === 'submit'
-    && email.value === email.value.toLowerCase()
+    target.id === "submit" &&
+    email.value === email.value.toLowerCase()
   ) {
-    emailError.style.visibility = 'hidden';
+    emailError.style.visibility = "hidden";
     inputObj.inputEmail = email.value;
     inputObj.inputName = inputname.value;
     localStorage.clear();
-    localStorage.setItem('object', JSON.stringify(inputObj));
+    localStorage.setItem("object", JSON.stringify(inputObj));
   }
-  if (target.id === 'cancel-popup') {
+  if (target.id === "cancel-popup") {
     e.preventDefault();
     document
-      .querySelector('.popup-container')
-      .classList.remove('display-popup');
+      .querySelector(".popup-container")
+      .classList.remove("display-popup");
     document
-      .querySelector('.popup-container')
-      .classList.remove('display-popup-desktop');
-    document.querySelector('.blur').classList.remove('show-blur');
-    document.querySelector('body').classList.remove('no-overflow');
+      .querySelector(".popup-container")
+      .classList.remove("display-popup-desktop");
+    document.querySelector(".blur").classList.remove("show-blur");
+    document.querySelector("body").classList.remove("no-overflow");
   }
-  if (target.id === 'see-all') {
+  if (target.id === "see-all") {
     e.preventDefault();
-    works.innerHTML = '';
+    works.innerHTML = "";
     defaultLoopValue = projectCards.length;
     createCards();
-    e.target.style.display = 'none';
+    e.target.style.display = "none";
   }
 });
